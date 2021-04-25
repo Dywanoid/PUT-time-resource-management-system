89c3bdacf54f6031e93b2630a4f6610d5346249a
Tomasz Bekas <tomasz.bekas@gmail.com>
2021-04-25 16:08:20 +0200

    API for client projects and tasks

--- backend/database.py
+++ backend/database.py
@@ -1,15 +1,31 @@
 from flask_sqlalchemy import SQLAlchemy
 from app import app
 
+
 db = SQLAlchemy(app)
 
 
 class Client(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+    tax_id = db.Column(db.String)
+    street_with_number = db.Column(db.String)
+    zip_code = db.Column(db.String)
+    city = db.Column(db.String)
+    created_at = db.Column(db.DateTime)
+    projects = db.relationship("Project", lazy='joined', order_by="desc(Project.created_at)")
 
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "name": self.name
-        }
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    client_id = db.Column(db.Integer, db.ForeignKey(Client.id), nullable=False)
+    name = db.Column(db.String)
+    created_at = db.Column(db.DateTime)
+    tasks = db.relationship("Task", lazy='joined', order_by="desc(Task.created_at)")
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    project_id = db.Column(db.Integer, db.ForeignKey(Project.id), nullable=False)
+    name = db.Column(db.String)
+    created_at = db.Column(db.DateTime)
