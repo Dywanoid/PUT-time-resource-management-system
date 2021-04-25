074759e3f90b1b00fb2c2674d70676c28e2eeb77
Tomasz Bekas <tomasz.bekas@gmail.com>
2021-04-25 17:27:27 +0200

    Enable CORS

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask, redirect, url_for, send_from_directory
 from flask_oidc import OpenIDConnect
 from flask_talisman import Talisman
+from flask_cors import CORS
 
 app = Flask(__name__, static_folder='/app/frontend/build')
 app.config.update({
@@ -12,8 +13,11 @@ app.config.update({
     'OIDC_SCOPES': ['openid', 'email', 'profile'],
     'OIDC_COOKIE_SECURE': os.getenv("OIDC_COOKIE_SECURE", True),
     'OIDC_CLIENT_SECRETS': os.getenv("OIDC_CLIENT_SECRETS", "local_client_secrets.json"),
-    'OVERWRITE_REDIRECT_URI': os.getenv("OVERWRITE_REDIRECT_URI", False)
+    'OVERWRITE_REDIRECT_URI': os.getenv("OVERWRITE_REDIRECT_URI", False),
+    'CORS_SUPPORTS_CREDENTIALS': True,
+    'CORS_ORIGINS': 'http://localhost*'
 })
+cors = CORS(app)
 oidc = OpenIDConnect(app)
 Talisman(app, content_security_policy=None)
 
