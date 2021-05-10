31de35cd79ae2a08a3cfc83a6f7288a4682f63e2
mariuszantas <antasmariusz1@gmail.com>
2021-05-10 21:42:20 +0200

    removed ^ from package.json + production check

--- frontend/src/graphql/apolloClient/index.ts
+++ frontend/src/graphql/apolloClient/index.ts
@@ -1,8 +1,10 @@
 import {ApolloClient, InMemoryCache, createHttpLink} from '@apollo/client';
 
+const IS_PRODUCTION = process.env.NODE_ENV === 'production';
+
 const link = createHttpLink({
   credentials: 'include',
-  uri: 'http://localhost/graphql'
+  uri: IS_PRODUCTION ?  `http://${ window.location.host }/graphql` : 'http://localhost/graphql'
 });
 
 const client = new ApolloClient({
