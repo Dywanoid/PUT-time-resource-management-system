8bdb1d7d208375e91f3dd9c8ea1c06c30b571b7c
mariuszantas <antasmariusz1@gmail.com>
2021-07-11 14:13:28 +0200

    New project assignments view

--- frontend/src/views/index.tsx
+++ frontend/src/views/index.tsx
@@ -4,7 +4,7 @@ export { ApplicationsView } from './ApplicationsView';
 export { ClientsView } from './ClientsView/ClientsView';
 export { ProjectsView } from './ProjectsView/ProjectsView';
 export { TaskView } from './TasksView/TasksView';
-export { ProjectAssignmentsView } from './ProjectAssignmentsView';
+export { ProjectAssignmentsView } from './projectAssignmentsView/ProjectAssignmentsView';
 export { TeamsView } from './TeamsView';
 export { ReportsView } from './ReportsView';
 export { SubordinatesView } from './SubordinatesView';
